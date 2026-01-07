e04bf16342c4a304121a341d5160c99d88760be3
euniceebube93-tech <euniceebube93@gmail.com>
2026-01-07 14:04:01 +0100

    Updated config.ts

--- services/config.ts
+++ services/config.ts
@@ -4,6 +4,7 @@
 // Your Backend URL - FOR LOCAL DEVELOPMENT USE LOCALHOST
 export const BACKEND_URL = "http://localhost:5000";
 
+export const BACKEND_URL = "https://acenexacbt.onrender.com";
 // SET THIS TO TRUE TO DISABLE BACKEND AND USE LOCAL STORAGE ONLY
 // Change to 'false' when you want to connect to the real server.
 export const FORCE_OFFLINE = false;
